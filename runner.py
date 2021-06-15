52cde0d8c57aa347b94b03b37d4eab0aa1945526
maxuanquang <maxuanquang@gmail.com>
2021-06-15 10:52:37 +0700

    Update runner.py

--- runner.py
+++ runner.py
@@ -10,9 +10,6 @@ parser.add_argument('--test', action='store_true')
 parser.add_argument('--infer', action='store_true')
 
 parser.add_argument('--config-path', dest='config_path', type=str, default='config', required=True)
-# parser.add_argument('--epochs', type=int, default=1, required=True)
-# parser.add_argument('--epoch-size', type=int, default=100, required=True)
-# parser.add_argument('--name', dest='name', type=str, default='demo', required=False)
 
 def main():
     global best_error, n_iter, device
@@ -25,8 +22,8 @@ def main():
         sfmlearner.test()
     elif args.infer:
         sfmlearner.infer()
-    elif args.prepare_dataset:
-        sfmlearner.prepare_dataset()
+    # elif args.prepare_dataset:
+    #     sfmlearner.prepare_dataset()
 
 if __name__ == '__main__':
     main()
