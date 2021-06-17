e3f0515c741e9632b3fd7525b059a0eebde1b88f
maxuanquang <maxuanquang@gmail.com>
2021-06-17 11:00:52 +0700

    Change OptimizerCreator.create()

--- OptimizerCreator.py
+++ OptimizerCreator.py
@@ -6,13 +6,13 @@ class OptimizerCreator():
     def __init__(self, args):
         self.args = args
 
-    def create(self, disp_net, pose_exp_net):
+    def create(self, *networks):
         print('=> setting adam solver')
-        print(type(self.args.lr))
-        optim_params = [
-            {'params': disp_net.parameters(), 'lr': self.args.lr},
-            {'params': pose_exp_net.parameters(), 'lr': self.args.lr}
-        ]
+        
+        optim_params = []
+        for item in networks:
+            optim_params.append({'params': item.parameters(), 'lr': self.args.lr})
+
         optimizer = torch.optim.Adam(optim_params,
                                     betas=(self.args.momentum, self.args.beta),
                                     weight_decay=self.args.weight_decay)
